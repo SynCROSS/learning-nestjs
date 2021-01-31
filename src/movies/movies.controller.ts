b2b0b5b69c3d78d25c355071982ddc4d0599e95d
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-01-31 23:08:40 +0900

    Decorators about Routing

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,5 +1,19 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { title } from 'process';
 
+interface MovieData {
+  title: string;
+  description: string;
+}
 @Controller('movies')
 export class MoviesController {
   movies = [
@@ -25,8 +39,60 @@ export class MoviesController {
     return this.movies;
   }
 
-  @Get('/:id')
+  // * Why It put this on top of get(':id') is
+  // * because all get methods under get(:id) are
+  // * recognized as get(:id) methods.
+  // * This Problem occurs when using express.js
+  @Get('search')
+  searchMovieByTitle(@Query('title') title: string) {
+    const filteredMovie = this.movies.filter(movie => title === movie.title);
+
+    return filteredMovie;
+  }
+
+  @Get(':id')
   getMovieById(@Param('id') id: number) {
-    return this.movies[id - 1];
+    return this.movies[+id - 1];
+  }
+
+  @Post('add')
+  addMovie(@Body() movieData: MovieData) {
+    for (const movie of this.movies) {
+      if (movieData.title === movie.title) {
+        return null;
+      }
+    }
+
+    const newMovie = {
+      id: this.movies.length + 1,
+      title: movieData.title,
+      description: movieData.description,
+    };
+
+    this.movies.push(newMovie);
+
+    return newMovie;
+  }
+
+  @Patch(':id')
+  updateMovieById(@Param('id') id: number, @Body() movieData?: MovieData) {
+    this.movies[+id - 1] = {
+      id,
+      title: movieData.title,
+      description: movieData.description,
+    };
+
+    return this.movies;
+  }
+
+  @Delete(':id')
+  removeMovieById(@Param('id') id: number): boolean {
+    const filteredMovies = this.movies.filter(movie => +id !== movie.id);
+
+    if (this.movies.length > filteredMovies.length) {
+      this.movies = filteredMovies;
+      return true;
+    }
+    return false;
   }
 }
