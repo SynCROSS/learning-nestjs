b7c36d2d98b1949f9d4d041d0bf68958df084407
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-02-01 21:22:05 +0900

    Controller is for routing

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,3 +1,5 @@
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
 import {
   Body,
   Controller,
@@ -8,90 +10,39 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { MovieDataDTO } from './dto/MovieData.dto';
 
-interface MovieData {
-  title: string;
-  description: string;
-}
 @Controller('movies')
 export class MoviesController {
-  movies = [
-    {
-      id: 1,
-      title: 'title1',
-      description: 'description1',
-    },
-    {
-      id: 2,
-      title: 'title2',
-      description: 'description2',
-    },
-    {
-      id: 3,
-      title: 'title3',
-      description: 'description3',
-    },
-  ];
+  constructor(private readonly moviesService: MoviesService) {}
 
   @Get()
-  getAllMovies() {
-    return this.movies;
+  getAllMovies(): Movie[] {
+    return this.moviesService.getAllMovies();
   }
 
-  // * Why It put this on top of get(':id') is
-  // * because all get methods under get(:id) are
-  // * recognized as get(:id) methods.
-  // * This Problem occurs when using express.js
   @Get('search')
   searchMovieByTitle(@Query('title') title: string) {
-    const filteredMovie = this.movies.filter(movie => title === movie.title);
-
-    return filteredMovie;
+    this.moviesService.searchMovieByTitle(title);
   }
 
   @Get(':id')
-  getMovieById(@Param('id') id: number) {
-    return this.movies[+id - 1];
+  getMovieById(@Param('id') id: number): Movie {
+    return this.moviesService.getMovieById(+id);
   }
 
   @Post('add')
-  addMovie(@Body() movieData: MovieData) {
-    for (const movie of this.movies) {
-      if (movieData.title === movie.title) {
-        return null;
-      }
-    }
-
-    const newMovie = {
-      id: this.movies.length + 1,
-      title: movieData.title,
-      description: movieData.description,
-    };
-
-    this.movies.push(newMovie);
-
-    return newMovie;
+  addMovie(@Body() movieData: MovieDataDTO) {
+    return this.moviesService.addMovie(movieData);
   }
 
   @Patch(':id')
-  updateMovieById(@Param('id') id: number, @Body() movieData?: MovieData) {
-    this.movies[+id - 1] = {
-      id,
-      title: movieData.title,
-      description: movieData.description,
-    };
-
-    return this.movies;
+  updateMovieById(@Param('id') id: number, @Body() movieData: MovieDataDTO) {
+    return this.moviesService.updateMovieById(id, movieData);
   }
 
   @Delete(':id')
   removeMovieById(@Param('id') id: number): boolean {
-    const filteredMovies = this.movies.filter(movie => +id !== movie.id);
-
-    if (this.movies.length > filteredMovies.length) {
-      this.movies = filteredMovies;
-      return true;
-    }
-    return false;
+    return this.moviesService.removeMovieById(+id);
   }
 }
