61792f17734c1004f1df2be679541efa11c05e85
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-02-03 21:42:16 +0900

    Add Unit Test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -14,5 +15,88 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAllMovies()', () => {
+    it('should returns the array', () => {
+      expect(service.getAllMovies()).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('searchMovieByTitle(title)', () => {
+    it('should return Movie', () => {
+      expect(service.searchMovieByTitle('title1').title).toEqual('title1');
+    });
+  });
+
+  describe('getMovieById(id)', () => {
+    it('should return Movie', () => {
+      expect(service.getMovieById(1).id).toEqual(1);
+    });
+
+    it('should throws the 404 error', () => {
+      try {
+        service.getMovieById(404);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toEqual(
+          `Movie matching that id 404 is not found`,
+        );
+      }
+    });
+  });
+
+  describe('addMovie(MovieData)', () => {
+    it('should return Movie', () => {
+      expect(
+        service.addMovie({
+          title: 'title',
+          description: 'description',
+          year: 2021,
+          genres: ['serneg'],
+        }),
+      ).toEqual({
+        id: 4,
+        title: 'title',
+        description: 'description',
+        year: 2021,
+        genres: ['serneg'],
+      });
+    });
+  });
+
+  describe('updateMovieById(id, MovieData)', () => {
+    it('should return Movie', () => {
+      service.addMovie({
+        title: 'title',
+        description: 'description',
+        year: 2021,
+        genres: ['serneg'],
+      });
+      expect(
+        service.updateMovieById(4, {
+          title: 'title4',
+          description: 'description4',
+          year: 3,
+        }),
+      ).toEqual({
+        id: 4,
+        title: 'title4',
+        description: 'description4',
+        year: 3,
+        genres: ['serneg'],
+      });
+    });
+  });
+  describe('removeMovieById(id)', () => {
+    it('should return boolean', () => {
+      service.addMovie({
+        title: 'title',
+        description: 'description',
+        year: 2021,
+        genres: ['serneg'],
+      });
+      expect(service.removeMovieById(4)).toBe(true);
+    });
   });
 });
